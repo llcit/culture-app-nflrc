2feda0096c47da3d4b22ad2bedee87813943de13
aitor-arronte <aitoraalvarez@gmail.com>
2022-06-16 14:12:49 -1000

    added API config

---
+++ config/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import uuid
+
+class Config(models.Model):
+	device_name = models.CharField(max_length=155, blank=False)
+	ipaddress = models.IPAddressField(unique=True, blank=False)
+	key = models.CharField(max_length=155,unique=True, default=uuid.uuid4(), blank=False)
+
+	def __str__(self):
+		return self.device_name
+
+
+class TokenApi(models.Model):
+	device = models.ForeignKey(Config)
+	token = models.CharField(255, blank=False)
+	valid_until = models.DateTimeField(blank=False)
+
+	def __str__(self):
+		return self.device.device_name
+
---
+++ config/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.decorators import login_required
+from .models import Config, TokenApi
+import hashlib
+import random
+import datetime
+
+@login_required
+def verify_device(config):
+	if Config.objects.filter(ipaddress=config['ip'], key=config['key']).exists():
+		obj = Config.objects.get(ipaddress=config['ip'], key=config['key'])
+		s = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz!.#') for i in range(59))
+		try:
+			hash = hashlib.sha256(s.encode('utf-8'))
+			TokenApi.objects.create(device=obj, token=hash, valid_until=datetime.datetime.now()+datetime.timedelta(days=2))
+			return {'message': 'OK', 'token': s}
+		except:
+			return {'message': 'KO'}
+	else:
+		return {'message': 'KO'}
+
+
+def token_is_valid(token):
+	hash = hashlib.sha256(token.encode('utf-8'))
+	try:
+		tk = TokenApi.objects.get(token= hash)
+		if tk.valid_until > datetime.datetime.now():
+			return True
+		else:
+			return False
+	except:
+		return False
+
+
+
