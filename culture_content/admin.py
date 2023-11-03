6278d0ae570c2645d09df8ad4a85573a0380c819
Richard Medina <rmedina@hawaii.edu>
2023-11-02 16:29:34 -1000

    added foreign key object to display list

--- culture_content/admin.py
+++ culture_content/admin.py
@@ -48,6 +48,22 @@ class ModuleAdmin(admin.ModelAdmin):
         model = Module
         exclude = []
 
+    #### Display Learning Objective in object list ####
+    search_fields = ('name',)
+    list_display_links = ('name',)
+    list_filter = ('name',)
+    list_display = ('id', 'name', 'display_learning_obj',)
+
+    def display_learning_obj(self, obj):
+        try:
+            link = reverse("admin:culture_content_learningobjectives_change", args=[obj.objectives.id])
+            return format_html('<a href="{}">{}</a>', link, obj.objectives)
+        except:
+            return 'Unspecified'
+
+    display_learning_obj.short_description = "Related Learning Objective"
+    #### END Display Learning Objective in object list ####
+
     formfield_overrides = {
 
         models.TextField: {'widget': TinyMCE()}
@@ -62,15 +78,21 @@ class TopicAdmin(admin.ModelAdmin):
         model = Topic
         exclude = []
 
+    #### Display Learning Objective in object list ####
     search_fields = ('name',)
-    list_display = ('id', 'name', 'display_learning_obj',)
     list_display_links = ('name',)
     list_filter = ('name',)
+    list_display = ('id', 'name', 'display_learning_obj',)
 
     def display_learning_obj(self, obj):
-        link = reverse("admin:culture_content_learningobjectives_change", args=[obj.objectives.id])
-        return format_html('<a href="{}">{}</a>', link, obj.objectives)
+        try:
+            link = reverse("admin:culture_content_learningobjectives_change", args=[obj.objectives.id])
+            return format_html('<a href="{}">{}</a>', link, obj.objectives)
+        except:
+            return 'Unspecified'
+
     display_learning_obj.short_description = "Related Learning Objective"
+    #### END Display Learning Objective in object list ####
 
     scenarios = forms.ModelMultipleChoiceField(queryset=Topic.objects.all(),
                                                required=True,
@@ -112,8 +134,22 @@ class ScenarioAdmin(admin.ModelAdmin):
         model = Scenario
         exclude = []
 
+    #### Display Judgement Task in object list ####
     search_fields = ('name',)
-    list_display = ('name',)
+    list_display_links = ('name',)
+    list_filter = ('name',)
+    list_display = ('id', 'name', 'display_judgement_task',)
+
+    def display_judgement_task(self, obj):
+        try:
+            link = reverse("admin:culture_content_judgmenttask_change", args=[obj.judgment_task.id])
+            return format_html('<a href="{}">{}</a>', link, obj.judgment_task)
+        except:
+            return 'Unspecified'
+
+    display_judgement_task.short_description = "Related Judgement Task"
+    #### END Display Judgement Task in object list ####
+
     formfield_overrides = {
 
         models.TextField: {'widget': TinyMCE()}
