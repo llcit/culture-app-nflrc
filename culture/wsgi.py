31bd2c303401ecbacceb3df0a8d6ffdc7cd50530
Richard Medina <rmedina@hawaii.edu>
2025-11-04 13:51:47 -1000

    adjusting import script

--- culture/wsgi.py
+++ culture/wsgi.py
@@ -11,5 +11,6 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'culture.settings')
+
+application = get_wsgi_application()
